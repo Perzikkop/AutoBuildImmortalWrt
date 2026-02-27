7b1bb1cc1d38107d43dae895d0b1a501c4e5bab2
Perzikkop <123@gmailcom>
2026-02-27 08:38:05 +0800

    update

--- mediatek-filogic/build24.sh
+++ mediatek-filogic/build24.sh
@@ -1,21 +1,41 @@
 #!/bin/bash
-# Log file for debugging
 echo "🔄 修改软件源为镜像源..."
 
 sed -i 's|https://downloads.immortalwrt.org|https://mirrors.sjtug.sjtu.edu.cn/immortalwrt|g' \
     /home/build/immortalwrt/repositories.conf
-    
-source shell/custom-packages.sh
-echo "第三方软件包: $CUSTOM_PACKAGES"
-LOGFILE="/tmp/uci-defaults-log.txt"
-echo "Starting 99-custom.sh at $(date)" >> $LOGFILE
-echo "编译固件大小为: $PROFILE MB"
-echo "Include Docker: $INCLUDE_DOCKER"
 
+source shell/custom-packages.sh
+# 该文件实际为imagebuilder容器内的build.sh
+
+#echo "✅ 你选择了第三方软件包：$CUSTOM_PACKAGES"
+# 下载 run 文件仓库
+echo "🔄 正在同步第三方软件仓库 Cloning run file repo..."
+git clone --depth=1 https://github.com/wukongdaily/store.git /tmp/store-run-repo
+
+# 拷贝 run/arm64 下所有 run 文件和ipk文件 到 extra-packages 目录
+mkdir -p /home/build/immortalwrt/extra-packages
+cp -r /tmp/store-run-repo/run/arm64/* /home/build/immortalwrt/extra-packages/
+
+echo "✅ Run files copied to extra-packages:"
+ls -lh /home/build/immortalwrt/extra-packages/*.run
+# 解压并拷贝ipk到packages目录
+sh shell/prepare-packages.sh
+ls -lah /home/build/immortalwrt/packages/
+# 添加架构优先级信息
+sed -i '1i\
+arch aarch64_generic 10\n\
+arch aarch64_cortex-a53 15' repositories.conf
+
+
+
+# yml 传入的路由器型号 PROFILE
+echo "Building for profile: $PROFILE"
+
+echo "Include Docker: $INCLUDE_DOCKER"
 echo "Create pppoe-settings"
 mkdir -p  /home/build/immortalwrt/files/etc/config
 
-# 创建pppoe配置文件 yml传入环境变量ENABLE_PPPOE等 写入配置文件 供99-custom.sh读取
+# 创建pppoe配置文件 yml传入pppoe变量————>pppoe-settings文件
 cat << EOF > /home/build/immortalwrt/files/etc/config/pppoe-settings
 enable_pppoe=${ENABLE_PPPOE}
 pppoe_account=${PPPOE_ACCOUNT}
@@ -25,27 +45,9 @@ EOF
 echo "cat pppoe-settings"
 cat /home/build/immortalwrt/files/etc/config/pppoe-settings
 
-if [ -z "$CUSTOM_PACKAGES" ]; then
-  echo "⚪️ 未选择 任何第三方软件包"
-else
-  # ============= 同步第三方插件库==============
-  # 同步第三方软件仓库run/ipk
-  echo "🔄 正在同步第三方软件仓库 Cloning run file repo..."
-  git clone --depth=1 https://github.com/wukongdaily/store.git /tmp/store-run-repo
-
-  # 拷贝 run/x86 下所有 run 文件和ipk文件 到 extra-packages 目录
-  mkdir -p /home/build/immortalwrt/extra-packages
-  cp -r /tmp/store-run-repo/run/x86/* /home/build/immortalwrt/extra-packages/
-
-  echo "✅ Run files copied to extra-packages:"
-  ls -lh /home/build/immortalwrt/extra-packages/*.run
-  # 解压并拷贝ipk到packages目录
-  sh shell/prepare-packages.sh
-  ls -lah /home/build/immortalwrt/packages/
-fi
-
 # 输出调试信息
-echo "$(date '+%Y-%m-%d %H:%M:%S') - 开始编译..."
+echo "$(date '+%Y-%m-%d %H:%M:%S') - Starting build process..."
+
 
 # 定义所需安装的包列表 下列插件你都可以自行删减
 PACKAGES=""
@@ -73,15 +75,20 @@ PACKAGES="$PACKAGES script-utils"
 PACKAGES="$PACKAGES luci-i18n-samba4-zh-cn"
 
 
+# 第三方软件包 合并
 # ======== shell/custom-packages.sh =======
-# 合并imm仓库以外的第三方插件
-PACKAGES="$PACKAGES $CUSTOM_PACKAGES"
+if [ "$PROFILE" = "glinet_gl-axt1800" ] || [ "$PROFILE" = "glinet_gl-ax1800" ]; then
+    # 这2款 暂时不支持第三方插件的集成 snapshot版本太高 opkg换成apk包管理器 6.12内核 
+    echo "Model:$PROFILE not support third-parted packages"
+    PACKAGES="$PACKAGES -luci-i18n-diskman-zh-cn luci-i18n-homeproxy-zh-cn"
+else
+    echo "Other Model:$PROFILE"
+    PACKAGES="$PACKAGES $CUSTOM_PACKAGES"
+fi
 
 # 判断是否需要编译 Docker 插件
 if [ "$INCLUDE_DOCKER" = "yes" ]; then
     PACKAGES="$PACKAGES luci-i18n-dockerman-zh-cn"
-    PACKAGES="$PACKAGES iptables-nft" 
-    PACKAGES="$PACKAGES ip6tables-nft"
     echo "Adding package: luci-i18n-dockerman-zh-cn"
 fi
 
@@ -90,7 +97,7 @@ if echo "$PACKAGES" | grep -q "luci-app-openclash"; then
     echo "✅ 已选择 luci-app-openclash，添加 openclash core"
     mkdir -p files/etc/openclash/core
     # Download clash_meta
-    META_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-amd64.tar.gz"
+    META_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-arm64.tar.gz"
     wget -qO- $META_URL | tar xOvz > files/etc/openclash/core/clash_meta
     chmod +x files/etc/openclash/core/clash_meta
     # Download GeoIP and GeoSite
@@ -100,16 +107,6 @@ else
     echo "⚪️ 未选择 luci-app-openclash"
 fi
 
-# 若构建Nikki 则添加Geo文件
-if echo "$PACKAGES" | grep -q "luci-i18n-nikki-zh-cn"; then
-    echo "✅ 已选择 Nikki，添加 Geo文件"
-    mkdir -p files/etc/nikki/run
-    # Download GeoIP and GeoSite
-    wget -q https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat -O files/etc/nikki/run/GeoIP.dat
-    wget -q https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat -O files/etc/nikki/run/GeoSite.dat
-else
-    echo "⚪️ 未选择 Nikki"
-fi
 
 # 构建镜像
 echo "$(date '+%Y-%m-%d %H:%M:%S') - Building image with the following packages:"
